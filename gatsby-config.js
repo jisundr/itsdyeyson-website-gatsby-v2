987ff1a658377f7459530182a6bd6bfd8c70008c
Jayson De los Reyes <dyeyson.delosreyes@gmail.com>
2020-01-23 01:33:23 +0800

    style: add favicon and update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jayson De los Reyes: Web Developer`,
+    description: `I'm Jayson De los Reyes, a software engineer and web developer from the Philippines.`,
+    author: `@itsdyeyson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `itsdyeyson`,
+        short_name: `itsdyeyson`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#011627`,
+        theme_color: `#011627`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/itsdyeyson-icon.png`,
       },
     },
     {
