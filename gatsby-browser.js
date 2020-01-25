04a03e258817761432ab9d0517d33fb9d56bae80
Jayson De los Reyes <dyeyson.delosreyes@gmail.com>
2020-01-25 11:55:04 +0800

    chore: add whyDidYouRender

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+import React from "react"
+
+if (process.env.NODE_ENV !== "production") {
+  const whyDidYouRender = require("@welldone-software/why-did-you-render")
+  whyDidYouRender(React)
+}
+
 export { wrapRootElement } from "./src/wrapRootElement"
 
 // You can delete this file if you're not using it
